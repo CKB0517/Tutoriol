3374a63afc06be0460df29e55b392b4d78ade694
Johnson Feng <johnson1996johnson1996@gmail.com>
2017-03-10 11:03:20 +0800

    fifth commit

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 #include <string>
 #include <iostream>
+
 using namespace System;
+
 bool Guess(int number) {
 	static int target = -1;
 	if (target == -1) {
@@ -16,14 +18,15 @@ bool Guess(int number) {
 		return true;
 	}
 	else std::cout << "Wrong" << std::endl;
-	return false;
+	return false;
+
 	return 0;
 }
 int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 - 100 :";
+		std::cout << "Choose a number between 1 - 100000 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
