ff9f1116baaba273badeca775dbd4f8d02c8a490
CKB0517 <beautifulmontion@gmail.com>
2017-03-10 11:04:01 +0800

    six commit

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,14 +16,15 @@ bool Guess(int number) {
 		return true;
 	}
 	else std::cout << "Wrong" << std::endl;
-	return false;
+	return false;
+
 	return 0;
 }
 int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 - 100 :";
+		std::cout << "Choose a number between 3 - 500 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
